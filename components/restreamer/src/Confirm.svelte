805c08960752474e26536b34789c1b268c309910
PavelKushnirchuk <kushnirchuk@gmail.com>
2021-04-17 16:47:35 +0300

    Do not close modals by clicking outside of it

--- components/restreamer/src/Confirm.svelte
+++ components/restreamer/src/Confirm.svelte
@@ -14,19 +14,13 @@
     functionToCall = { func, args };
     showDialog = true;
   }
-
-  function onAreaClick(event) {
-    if (event.target.classList.contains('uk-modal')) {
-      showDialog = false;
-    }
-  }
 </script>
 
 <template>
   <slot {confirm} />
 
   {#if showDialog}
-    <div class="uk-modal uk-open" on:click={onAreaClick}>
+    <div class="uk-modal uk-open">
       <div class="uk-modal-dialog uk-modal-body">
         <h2 class="uk-modal-title">
           <slot name="title">
