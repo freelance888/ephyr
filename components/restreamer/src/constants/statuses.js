17bbcc31a9c72b2b7dd61126202a2c084d202f71
Igor Buts <ibuts@star.global>
2021-08-14 15:54:50 +0300

    Run fmt script

--- components/restreamer/src/constants/statuses.js
+++ components/restreamer/src/constants/statuses.js
@@ -1,5 +1,5 @@
-export const OFFLINE = 'OFFLINE'
-export const INITIALIZING = 'INITIALIZING'
-export const ONLINE = 'ONLINE'
+export const OFFLINE = 'OFFLINE';
+export const INITIALIZING = 'INITIALIZING';
+export const ONLINE = 'ONLINE';
 
 export const statusesList = [OFFLINE, INITIALIZING, ONLINE];
