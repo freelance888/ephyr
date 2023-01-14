7ea9be1332f237ad46bc825a6dfef7dc26cf01ca
Serhii Khalymon <sergiykhalimon@gmail.com>
2023-01-14 12:59:20 +0100

    Implement monitoring for jaeger

--- common/log/src/lib.rs
+++ common/log/src/lib.rs
@@ -18,13 +18,17 @@
     unused_qualifications,
     unused_results
 )]
-
-use tokio::io::{AsyncBufReadExt, AsyncRead, BufReader};
 pub use tracing::{self, Level};
 pub use tracing_actix_web;
 pub use tracing_log::log;
+
+use opentelemetry::sdk::{
+    propagation::TraceContextPropagator,
+    trace::{Config, Sampler},
+};
+use tokio::io::{AsyncBufReadExt, AsyncRead, BufReader};
 use tracing_log::LogTracer;
-use tracing_subscriber::FmtSubscriber;
+use tracing_subscriber::{layer::SubscriberExt, FmtSubscriber};
 
 /// Initializes global logger with the given verbosity `level` ([`Info`] by
 /// default, if [`None`]), returning its guard that should be held as long as
@@ -40,7 +44,24 @@ pub fn init(level: Option<Level>) {
         panic!("Failed to initialize logger: {e}");
     };
     let level = level.unwrap_or(Level::INFO);
-    let subscriber = FmtSubscriber::builder().with_max_level(level).finish();
+
+    // Install a new OpenTelemetry trace pipeline
+    // let tracer = stdout::new_pipeline().install_simple();
+    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_service_name("local-ephyr")
+        .with_endpoint("5.161.113.45:6831")
+        .install_simple()
+        .unwrap();
+    opentelemetry::global::set_text_map_propagator(
+        TraceContextPropagator::new(),
+    );
+
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(level)
+        .finish()
+        .with(telemetry);
+    // add tracing logging for stdout in addition to opentelemetry
     tracing::subscriber::set_global_default(subscriber)
         .expect("setting tracing subscriber failed");
 }
