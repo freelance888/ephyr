f440d9eb3664fea838d2f2fd6ab46dfd927ee85e
Serhii Khalymon <serhii.khalymon@pm.me>
2023-08-06 13:34:32 +0200

    Try to fix docker build

--- components/restreamer/src/api/mod.rs
+++ components/restreamer/src/api/mod.rs
@@ -1,5 +1,6 @@
 //! APIs used in application or provided by it.
 
+// Definitions of APIs provided by Google Drive V3.
 pub use ephyr_api_google_drive as google_drive;
 pub mod graphql;
 pub mod srs;
